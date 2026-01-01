8e620f796c2ab1a98a1e1921452f62e916cd543f
happymimimix <happymimimix@gmail.com>
2026-01-01 08:41:01 -0500

    Signed-off-by: happymimimix <happymimimix@gmail.com>

--- RedStarOS_3.5/v3.5_Update/scripts/1.sh
+++ RedStarOS_3.5/v3.5_Update/scripts/1.sh
@@ -21,6 +21,7 @@ Install libatomic_ops-7.6.2 gz
 Install gc-7.6.2 gz
 Install guile-2.0.14 gz
 Install gpref-3.1 gz
+Install coreutils-7.6 xz
 Install autogen-5.18.7 xz
 Install zlib-1.2.11 xz
 InstallRoot zlib-1.2.11 xz
